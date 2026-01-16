f8efea5ba9d7500223030243d1327603aec44e5b
akrishan11 <akashkrishan11@gmail.com>
2026-01-15 19:27:39 -0500

    changes for new schedulign guide but config needs to be fixed

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,8 @@
 import type { MDXComponents } from "mdx/types"
 import CourseGraph from "@/components/Courses/CourseGraph"
+import ElectivesRankingTable from "@/components/ElectivesRankingTable"
 
-const components: MDXComponents = { CourseGraph }
+const components: MDXComponents = { CourseGraph, ElectivesRankingTable }
 
 export function useMDXComponents(): MDXComponents {
   return components
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,35 @@
 import createMDX from "@next/mdx"
 
+import path from "path"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    })
+
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: "raw-loader",
+    })
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+    }
+
+    // Fix for @dagrejs/dagre requiring @dagrejs/graphlib
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+
+    return config
+  },
 }
 
 const withMDX = createMDX({
